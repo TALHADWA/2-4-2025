a96a79a7c887f610a1f594adc9b22492e8480a83
Talha2023azzurri <talhaali21cv@gmail.com>
2025-02-06 00:40:04 -0800

    xc

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.post("/send_chatbot_message",upload.single("image"), async function (req,res
         // Trigger Pusher event (chatbot channel)
         pusher.trigger("chatbotchannel", "chatbot", {
             message: savedMessage.message,
-            messgaeType:savedMessage.messageType
+            messageType:savedMessage.messageType
             
           //  imageUrl: savedMessage.imageUrl || null // Send imageUrl if available
         });
